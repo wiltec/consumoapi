9e425e1934eb2c2a8f25e7a2d26a1a9955ae892b
wiltec <willtec.03@gmail.com>
2025-10-20 16:50:29 -0600

    Se crea modelo adapter y adaptador

---
+++ ConsumoAPI/app/src/main/java/com/simposiumtec/consumoapi/models/UseCfdiAdapterModel.kt
@@ -0,0 +1,9 @@
+package com.simposiumtec.consumoapi.models
+
+data class UseCfdiAdapterModel(
+    var IdUserCfdi: String = "",
+    var Description: String = "",
+    var CodeUseCfdi: String = "",
+    var AppliesPhysicalPerson: String = "",
+    var AppliesMoralPerson: String = ""
+)
